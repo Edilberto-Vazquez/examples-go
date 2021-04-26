d204ea92af97874d670985a27f363ffe9f8a0a7a
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-04-25 23:19:24 -0500

    packages

--- src/mypackage/mypackage.go
+++ src/mypackage/mypackage.go
@@ -1,7 +1,24 @@
+// para accesos publicos se escribe las clases, funciones y variables en mayusculas
 package mypackage
+
+import "fmt"
 
 // CarPublic Car con acceso publico
 type CarPublic struct {
 	Brand string
 	Year  int
+}
+
+type carPrivate struct {
+	brand string
+	year  int
+}
+
+// PrintMessage para imprimir un mensaje
+func PrintMessage(text string) {
+	fmt.Println(text)
+}
+
+func printMessage1(text string) {
+	fmt.Println(text)
 }
